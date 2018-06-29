8a56b9cd32802e64fa406a6346e7baffb80dd529
Jan Christoph Bischko <jbischko@barracuda.com>
2018-06-29 12:14:17 +0200

    hello cruel world

---
+++ hello_1st_world.c
@@ -0,0 +1,19 @@
+/* Prints part of a Lil Jon song text. */
+
+#include <stdlib.h>
+#include <stdio.h>
+
+int main( int argc, char * argv[] ) {
+	unsigned liljon;
+
+
+	printf(
+		"Fire up the loud, "
+		"another round of shots\n\n"
+	);
+
+	for( liljon=0; liljohn<10; liljon++ )
+		printf("Turn down for what?\n");
+
+	return 0
+}
